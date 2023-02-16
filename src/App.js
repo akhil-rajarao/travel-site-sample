8e92af37676f0467fec5be5635183ff4d04ec9c2
akhil k <akhil.rajarao@xelpmoc.in>
2023-02-16 16:34:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,259 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap mb-5 p-5 flex-col md:flex-row items-center">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-2xl">TravelBooking</span>
+          </a>
+          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a class="mr-5 hover:text-gray-900">Home</a>
+            <a class="mr-5 hover:text-gray-900">About</a>
+            <a class="mr-5 hover:text-gray-900">Blog</a>
+            <a class="mr-5 hover:text-gray-900">Contact</a>
+          </nav>
+          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            SignOut
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-1"
+              viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 mx-auto">
+            <div class="flex flex-col text-center mt-10 w-full mb-20">
+              <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+                TRAVEL BOOKING
+              </h2>
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                TICKET BOOKING SIMPLIFIED
+              </h1>
+              <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+                gentrify, subway tile poke farm-to-table. Franzen you probably
+                haven't heard of them man bun deep jianbing selfies heirloom
+                prism food truck ugh squid celiac humblebrag.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 mx-auto">
+            <div class="flex lg:w-2/3 w-fullsm:flex-row flex mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+              <div class="relativeflex-grow px-5 w-full">
+                <label
+                  for="full-name"
+                  class="leading-7 text-sm text-gray-600">
+                  FROM
+                </label>
+                <input
+                  type="text"
+                  id="full-name"
+                  name="full-name"
+                  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div class="relative flex-grow px-5 w-full">
+                <label
+                  for="email"
+                  class="leading-7 text-sm text-gray-600">
+                  TO
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div class="relative flex-grow px-5 w-full">
+                <label
+                  for="email"
+                  class="leading-7 text-sm text-gray-600">
+                  Date
+                </label>
+                <input
+                  type="date"
+                  id="email"
+                  name="email"
+                  class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Search
+              </button>
+            </div>
+          </div>
+        </section>
+        <section class="text-gray-600 body-font overflow-hidden">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="lg:w-4/5 mx-auto flex flex-wrap">
+              <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                <h2 class="text-sm title-font text-gray-500 tracking-widest">
+                  BRAND NAME
+                </h2>
+                <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">
+                  Best holiday package
+                </h1>
+                <div class="flex mb-4">
+                  <a class="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
+                    Description
+                  </a>
+                  <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
+                    Reviews
+                  </a>
+                  <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
+                    Details
+                  </a>
+                </div>
+                <p class="leading-relaxed mb-4">
+                  Fam locavore kickstarter distillery. Mixtape chillwave tumeric
+                  sriracha taximy chia microdosing tilde DIY. XOXO fam inxigo
+                  juiceramps cornhole raw denim forage brooklyn. Everyday carry
+                  +1 seitan poutine tumeric. Gastropub blue bottle austin
+                  listicle pour-over, neutra jean.
+                </p>
+                <div class="flex border-t border-gray-200 py-2">
+                  <span class="text-gray-500">Color</span>
+                  <span class="ml-auto text-gray-900">Blue</span>
+                </div>
+                <div class="flex border-t border-gray-200 py-2">
+                  <span class="text-gray-500">Size</span>
+                  <span class="ml-auto text-gray-900">Medium</span>
+                </div>
+                <div class="flex border-t border-b mb-6 border-gray-200 py-2">
+                  <span class="text-gray-500">Quantity</span>
+                  <span class="ml-auto text-gray-900">4</span>
+                </div>
+                <div class="flex">
+                  <span class="title-font font-medium text-2xl text-gray-900">
+                    $58.00
+                  </span>
+                  <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                    Button
+                  </button>
+                  <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      class="w-5 h-5"
+                      viewBox="0 0 24 24">
+                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <img
+                alt="ecommerce"
+                class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                src="https://images.unsplash.com/photo-1446160657592-4782fb76fb99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80"
+              />
+            </div>
+          </div>
+        </section>
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-col text-center w-full mb-20">
+              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                Active usage statistics
+              </h1>
+              <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+                gentrify, subway tile poke farm-to-table. Franzen you probably
+                haven't heard of them man bun deep jianbing selfies heirloom
+                prism food truck ugh squid celiac humblebrag.
+              </p>
+            </div>
+            <div class="flex flex-wrap -m-4 text-center">
+              <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                    viewBox="0 0 24 24">
+                    <path d="M8 17l4 4 4-4m-4-5v9"></path>
+                    <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+                  </svg>
+                  <h2 class="title-font font-medium text-3xl text-gray-900">
+                    2.7K
+                  </h2>
+                  <p class="leading-relaxed">Bookings</p>
+                </div>
+              </div>
+              <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                    viewBox="0 0 24 24">
+                    <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+                    <circle
+                      cx="9"
+                      cy="7"
+                      r="4"></circle>
+                    <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+                  </svg>
+                  <h2 class="title-font font-medium text-3xl text-gray-900">
+                    1.3K
+                  </h2>
+                  <p class="leading-relaxed">Users</p>
+                </div>
+              </div>
+
+              <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+                <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    class="text-indigo-500 w-12 h-12 mb-3 inline-block"
+                    viewBox="0 0 24 24">
+                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                  </svg>
+                  <h2 class="title-font font-medium text-3xl text-gray-900">
+                    Numerous
+                  </h2>
+                  <p class="leading-relaxed">Places</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </header>
     </div>
   );
